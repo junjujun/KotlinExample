d61756880657c902bbbd41a5ad9c59ae5f430ed0
junjujun <ftwlpsl@gmail.com>
2025-02-06 00:47:38 +0900

    1.5.4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,4 +3,9 @@ package org.example
 fun main() {
     val id: Int = 100
     var name: String = "Takehata"
+    print(countLength(name))
+}
+
+fun countLength(str: String): Int {
+    return str.length
 }

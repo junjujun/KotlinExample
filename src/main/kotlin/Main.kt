e8f97e858037c085a0f22038ddf6f74768e0767c
junjujun <ftwlpsl@gmail.com>
2025-02-08 19:16:51 +0900

    1.5.25

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,15 @@
 package org.example
 
 fun main() {
-    val androidPlatform = AndroidPlatform()
-    androidPlatform.showName()
-
-    IosPlatform().showName()
-
+    GreeterImpl().hello()
 }
 
-sealed class Platform {
-    abstract fun showName()
+interface Greeter {
+    fun hello()
 }
 
-class AndroidPlatform: Platform() {
-    override fun showName() {
-        println("Android.")
-    }
-}
-
-class IosPlatform: Platform() {
-    override fun showName() {
-        println("iOS.")
+class GreeterImpl: Greeter {
+    override fun hello() {
+        println("Hello.")
     }
 }

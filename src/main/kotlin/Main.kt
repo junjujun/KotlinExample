f4218c1d8c708d91becd2ee4400fe68735bd57e8
junjujun <ftwlpsl@gmail.com>
2025-02-08 15:25:46 +0900

    1.5.16

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,12 @@
 package org.example
 
 fun main() {
-    for (i in 1..10) {
-        println("i is $i")
-    }
-
-    for (i in 1 until 10 step 2) {
-        println("i is $i")
-    }
-
-    val list = listOf(1, 2, 5, 6, 10)
-    for (i in list) {
-        println("i is $i")
-    }
+    val human = Human()
+    human.showName("Takehata")
 }
 
+class Human {
+    fun showName(name: String) {
+        println(name)
+    }
+}

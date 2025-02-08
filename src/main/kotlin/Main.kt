498ac197732abc5d402e85f4edd4d4007ee98cb6
junjujun <ftwlpsl@gmail.com>
2025-02-08 19:13:28 +0900

    1.5.23

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,25 @@
 package org.example
 
-open class Animal(val name: String) {
-    init {
-        println("Animal init: $name")
-    }
-}
-
-class Dog(name: String) : Animal(name) {
-    var dogAge: Int
-
-    // セカンダリコンストラクタ
-    constructor(name: String, age: Int) : this(name) {
-        this.dogAge = age * 7
-        println("$name is $age years old. (Human age: $dogAge)")
-    }
-
-    init {
-        dogAge = 0 // プライマリコンストラクタ用のデフォルト値
-        println("Dog init block")
-    }
-}
-
 fun main() {
-    val dog1 = Dog("Buddy")            // プライマリコンストラクタ使用
-    val dog2 = Dog("Charlie", 3)       // セカンダリコンストラクタ使用
+    val androidPlatform = AndroidPlatform()
+    androidPlatform.showName()
+
+    IosPlatform().showName()
+
+}
+
+sealed class Platform {
+    abstract fun showName()
+}
+
+class AndroidPlatform: Platform() {
+    override fun showName() {
+        println("Android.")
+    }
+}
+
+class IosPlatform: Platform() {
+    override fun showName() {
+        println("iOS.")
+    }
 }
